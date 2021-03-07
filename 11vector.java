7459be5f28eb8715473782d13c9652e46ffad105
Manan Sharma <77826107+Manan10Sharma@users.noreply.github.com>
2021-03-06 23:54:17 -1000

    Create 11vector.java

---
+++ 11vector.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class fruits
+{
+public static void main(String args[])
+{
+Vector v=new Vector();
+int i;
+v.addElement("Mango");
+v.addElement("Apple");
+v.addElement("Pineapple");
+v.addElement("Orange");
+v.insertElementAt("kiwi",0);
+System.out.println("\n\nExample\n");
+for(i=0;i<v.size();i++)
+System.out.println(v.elementAt(i));
+System.out.println("First is "+v.firstElement());
+System.out.println("Last is "+v.lastElement());
+v.remove("apple");
+System.out.println("\n\n\t\t After removing apple list is");
+for(i=0;i<v.size();i++)
+System.out.println(v.elementAt(i));
+}
+}

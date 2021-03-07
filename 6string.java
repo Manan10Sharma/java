d96059f609330585c54f3c787f95ce0429bba937
Manan Sharma <77826107+Manan10Sharma@users.noreply.github.com>
2021-03-06 22:30:31 -1000

    Create 6string.java

---
+++ 6string.java
@@ -0,0 +1,12 @@
+class string
+{
+  public static void main(String args[])
+  {
+    String x="Airplane";
+    System.out.println("String:"+x);
+    System.out.println("Concat:"+x.concat("Mode"));
+    System.out.println("Length:"+x.length());
+    System.out.println("Replace:"+x.replace("l","t"));
+    System.out.println(x.tolower case());
+  }
+}

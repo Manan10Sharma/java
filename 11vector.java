2bd390ecc2732334587bd8c65209f1a394b4672d
Manan Sharma <77826107+Manan10Sharma@users.noreply.github.com>
2021-04-02 07:25:38 -1000

    Update 11vector.java

--- 11vector.java
+++ 11vector.java
@@ -15,7 +15,7 @@ for(i=0;i<v.size();i++)
 System.out.println(v.elementAt(i));
 System.out.println("First is "+v.firstElement());
 System.out.println("Last is "+v.lastElement());
-v.remove("apple");
+v.remove("Apple");
 System.out.println("\n\n\t\t After removing apple list is");
 for(i=0;i<v.size();i++)
 System.out.println(v.elementAt(i));

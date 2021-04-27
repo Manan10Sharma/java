8b3126926c36caf9f9e33d182cd60c203d46e2b5
Manan Sharma <77826107+Manan10Sharma@users.noreply.github.com>
2021-04-26 19:31:54 -1000

    Update Sum.java

--- Sum.java
+++ Sum.java
@@ -1,9 +1,11 @@
 import java.util.*;
-class sum{
-public static void main(String[] arts){
-a=12;
-b=13;
-c=a+b;
-System.out.println("Sum is"+c);
-}
-}
+class sum
+{
+  public static void main(String[] arts)
+  {
+    a=12;
+    b=13;
+    c=a+b;
+    System.out.println("Sum is"+c);
+    }
+  }

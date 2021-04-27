244e5bf7d23fd87c13680e456009473e96e72124
Manan Sharma <77826107+Manan10Sharma@users.noreply.github.com>
2021-04-26 18:46:30 -1000

    Update and rename 6string.java to string.java

--- string.java
+++ string.java
@@ -1,12 +1,12 @@
 class string
-{
-  public static void main(String args[])
   {
+  public static void main(String args[])
+    {
     String x="Airplane";
     System.out.println("String:"+x);
     System.out.println("Concat:"+x.concat("Mode"));
     System.out.println("Length:"+x.length());
     System.out.println("Replace:"+x.replace("l","t"));
     System.out.println(x.toLowerCase());
+    }
   }
-}

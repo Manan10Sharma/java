9bb4657af021f20eabc1b0bb9879dea00a98d025
Manan Sharma <77826107+Manan10Sharma@users.noreply.github.com>
2021-04-26 18:43:48 -1000

    Update and rename 4ser.java to series.java

---
+++ series.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class series
+  {
+  public static void main(String args[])
+    {
+    int j=1,i;
+    for(i=1;i<10;i++)
+      {
+      j=i*j;
+      System.out.print(j+",");
+      }
+    }
+  }

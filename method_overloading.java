044c21aa651bcf8d6944df40453e5572c60ea2f6
Manan Sharma <77826107+Manan10Sharma@users.noreply.github.com>
2021-04-26 19:45:56 -1000

    Update method_overloading.java

--- method_overloading.java
+++ method_overloading.java
@@ -1,33 +1,33 @@
-class maxi 
-{
-int x,y,z;
-void set() 
-{
-x=50; y=60;
-}
-void set(int i) 
-{
-x=i; y=100;
-}
-void set(int i,int j) 
-{
-x=i; y=j;
-}
-void show() 
-{
-if(x>y)
-System.out.println("\n max is:-"+x);
-else
-System.out.println("\n max is:-"+y);
-}
-}
-class exaover 
-{
-public static void main(String agrs[]) 
-{
-maxi ob=new maxi();
-ob.set(); ob.show();
-ob.set(300); ob.show();
-ob.set(17,6); ob.show();
-}
-}
+class maxi
+  {
+  int x,y,z;
+  void set()
+    {
+    x=50; y=60;
+    }
+  void set(int i)
+    {
+    x=i; y=100;
+    }
+  void set(int i,int j)
+    {
+    x=i; y=j;
+    }
+  void show()
+    {
+    if(x>y)
+      System.out.println("\n max is:-"+x);
+    else
+      System.out.println("\n max is:-"+y);
+    }
+  }
+class exaover
+  {
+  public static void main(String agrs[])
+    {
+    maxi ob=new maxi();
+    ob.set(); ob.show();
+    ob.set(300); ob.show();
+    ob.set(17,6); ob.show();
+    }
+  }

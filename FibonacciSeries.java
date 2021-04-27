067e2c62b6d7681a2c040c1e48c6d127b7d84b6e
Manan Sharma <77826107+Manan10Sharma@users.noreply.github.com>
2021-04-26 19:30:25 -1000

    Update FibonacciSeries.java

--- FibonacciSeries.java
+++ FibonacciSeries.java
@@ -1,18 +1,18 @@
 public class FibonacciSeries
-{
-public static void main(String args[])
-{
-int a,b,c,i,n;
-n=10;
-a=b=1;
-System.out.print(a+" "+b);
-for(i=1;i<=n-2;i++)
-{
-c=a+b;
-System.out.print(" ");
-System.out.print(c);
-a=b;
-b=c;
-}
-}
-}
+  {
+  public static void main(String args[])
+    {
+    int a,b,c,i,n;
+    n=10;
+    a=b=1;
+    System.out.print(a+" "+b);
+    for(i=1;i<=n-2;i++)
+      {
+      c=a+b;
+      System.out.print(" ");
+      System.out.print(c);
+      a=b;
+      b=c;
+      }
+    }
+  }

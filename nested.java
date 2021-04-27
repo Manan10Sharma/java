1e5da21235f2d132dd7b46d8d9b82ee802316a34
Manan Sharma <77826107+Manan10Sharma@users.noreply.github.com>
2021-04-26 18:44:58 -1000

    Update and rename 5nested.java to nested.java

---
+++ nested.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class nested
+  {
+  public static void main(String args[])
+    {
+    int i,j;
+    for(i=3;i>=1;i--)
+      {
+      for(j=i;j>=1;j--)
+        System.out.print(j);
+      System.out.println();
+      }
+    }
+  }

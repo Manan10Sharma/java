a11cf7c1e76187c49f57f76515431f08d0ae3fde
Manan Sharma <77826107+Manan10Sharma@users.noreply.github.com>
2021-04-26 19:40:19 -1000

    Update and rename interface to interface.java

--- interface.java
+++ interface.java
@@ -1,13 +1,16 @@
 import java.util.*;
-interface Detail {
-public void get();
-public void show(); 
-}
-class stud implements Detail {
+interface Detail
+  {
+  public void get();
+  public void show();
+  }
+class stud implements Detail
+{
 int r,p,a; 
 String n;
 Scanner in=new Scanner(System.in);
-public void get() { 
+public void get()
+{
 System.out.println("Student Name:-");
 n=in.nextLine();
 System.out.println("Student Roll no:-"); 
@@ -15,33 +18,41 @@ r=in.nextInt();
 System.out.println("Student Age:-"); 
 a=in.nextInt();
 System.out.println("Student Percentage:-"); 
-p=in.nextInt(); 
+p=in.nextInt();
 }
-public void show() {
+public void show() 
+{
 System.out.println("Student Name:-"+n+"\nStudent Roll no:-"+r+ "\nStudent Age:-"+a+ "\nStudent Percentage:-"+p);
-} 
 }
-class teacher implements Detail {
-int i,s; 
+}
+class teacher implements Detail
+{
+int i,s;
 String n=new String();
 Scanner in=new Scanner(System.in);
-public void get() {
-System.out.println("Teacher Name:-"); 
+public void get()
+{
+System.out.println("Teacher Name:-");
 n=in.nextLine();
 System.out.println("Teacher Id:-"); 
 i=in.nextInt();
 System.out.println("Teacher Salary:-"); 
 s=in.nextInt(); 
 }
-public void show() {
-System.out.println("Teacher Name:-"+n+"\nTeacher Id:-"+i+"\nTeacher Salary:-"+s); }}
-class detl {
-public static void main(String args[]) {
-teacher ob=new teacher();
-stud ob1=new stud();
-ob1.get(); 
-ob1.show();
-ob.get(); 
-ob.show(); 
+public void show() 
+{
+System.out.println("Teacher Name:-"+n+"\nTeacher Id:-"+i+"\nTeacher Salary:-"+s); 
 }
 }
+class detl
+{
+  public static void main(String args[])
+  {
+    teacher ob=new teacher();
+    stud ob1=new stud();
+    ob1.get();
+    ob1.show();
+    ob.get();
+    ob.show();
+    }
+  }
